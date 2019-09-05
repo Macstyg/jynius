3d72b81ebab59f784d745fa072627e747a31d4d8
Macstyg <macstyg@gmail.com>
2019-09-05 11:51:00 +0300

    test

--- src/lambda/test.js
+++ src/lambda/test.js
@@ -1,6 +1,6 @@
 export async function handler(event, context) {
   try {
-    console.log("event", event)
+    console.log("event", event.body.payload.event.name)
     console.log("context", context)
     return {
       statusCode: 200,
