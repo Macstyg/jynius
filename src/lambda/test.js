8d1affaaf4b5b365ea930453cea6fad00e505750
Macstyg <macstyg@gmail.com>
2019-09-05 12:47:30 +0300

    test

--- src/lambda/test.js
+++ src/lambda/test.js
@@ -1,12 +1,13 @@
 export async function handler(event, context) {
   try {
-    const payload = event.body.split("=")[1]
-    const normilizedPayload = decodeURI(payload)
+    const payloadString = event.body.split("=")[1]
+    const normilizedPayloadString = decodeURI(payloadString)
       .replace(/\+/g, " ")
       .replace(/%3A/g, ": ")
-    console.log("payload", normilizedPayload)
-    console.log("parsed payload", JSON.parse(normilizedPayload))
-    console.log("event", JSON.parse(normilizedPayload).event.name)
+    const payload = JSON.parse(normilizedPayloadString)
+    const name = payload.event.name
+    console.log("parsed payload", payload)
+    console.log("name", name)
     return {
       statusCode: 200,
       body: JSON.stringify({ name }),
